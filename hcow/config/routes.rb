915ad5f8b0b22eb15f200fe42b29a80c481e0ff9
Jayden Flanders <jm.flanders@baypath.net>
2023-01-06 17:36:41 +0000

    Initial Project

--- hcow/config/routes.rb
+++ hcow/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'site/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "site#home"
 end
