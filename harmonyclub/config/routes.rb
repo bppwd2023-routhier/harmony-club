6e5fc8c9116d7db6db930549c5d44a71a6ece503
Abrahm Routhier <aj.routhier@baypath.net>
2023-01-17 16:16:33 +0000

    actual working project

--- harmonyclub/config/routes.rb
+++ harmonyclub/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'site/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "site#home"
+  # root "articles#index"
 end
