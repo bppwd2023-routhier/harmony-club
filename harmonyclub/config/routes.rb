54161a01d7a206fe5a92697bd42dea68a1b540db
Jayden Flanders <jm.flanders@baypath.net>
2023-01-18 15:20:25 +0000

    devise views

--- harmonyclub/config/routes.rb
+++ harmonyclub/config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   resources :events
   resources :pages
   resources :sections
+  
+  devise_scope :user do
+    # Redirects signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
   devise_for :users
   get "pages/home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
