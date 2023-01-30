b2fe287c5088d18442ed286df320b7375a112629
Abrahm Routhier <aj.routhier@baypath.net>
2023-01-30 15:32:37 +0000

    very basic dynamic navbar (need to update later)

--- harmonyclub/app/controllers/application_controller.rb
+++ harmonyclub/app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :all_sections
+
+    def all_sections
+        @sections = Section.all
+    end
+
     def after_sign_in_path_for(resource)
         panel_path
     end
