97bdbbd84653f5fe6eaa25cc42902eec53cbd5db
Abrahm Routhier <aj.routhier@baypath.net>
2023-01-31 15:50:07 +0000

    Featured validation

--- harmonyclub/app/models/event.rb
+++ harmonyclub/app/models/event.rb
@@ -1,4 +1,11 @@
 class Event < ApplicationRecord
     has_one_attached :photo
     validates :title, :eventstart, :eventend, presence: true
+    validate :feature_validate
+
+    def feature_validate
+        if Event.where("featured==true").size >= 4
+            errors.add(:featured, "Cannot have more than 4 featured events") unless featured == false
+        end
+    end
 end
