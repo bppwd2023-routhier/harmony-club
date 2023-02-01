d1b8314f5311eb30c5c3c4d0ab1a189c6291b201
Jayden Flanders <jm.flanders@baypath.net>
2023-02-01 19:06:52 +0000

    Some permissions done

--- harmonyclub/app/controllers/application_controller.rb
+++ harmonyclub/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     include Pundit::Authorization
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
     before_action :all_sections
 
     def all_sections
@@ -8,5 +9,9 @@ class ApplicationController < ActionController::Base
 
     def after_sign_in_path_for(resource)
         panel_path
+    end
+    def user_not_authorized
+        flash[:alert] = "You are not authorized to perform this action."
+        redirect_back(fallback_location: root_path)
     end
 end
