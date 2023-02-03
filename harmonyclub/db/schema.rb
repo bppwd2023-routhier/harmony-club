fde1d2e7b4c2ed9a35ce81594ca79068f2095dae
Abrahm Routhier <aj.routhier@baypath.net>
2023-02-03 13:37:55 +0000

    pages working ?

--- harmonyclub/db/schema.rb
+++ harmonyclub/db/schema.rb
@@ -95,6 +95,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_17_163807) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean "dev"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
